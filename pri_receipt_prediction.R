e85507326226512b43516f7a84c69074392ebb7b
Bentley <bentley.hudson@henryschein.com>
2019-01-14 14:09:48 -0600

    columns added

--- pri_receipt_prediction.R
+++ pri_receipt_prediction.R
@@ -1,4 +1,4 @@
-#the training set is same as the validation set.  Need to modify based on other boosted tree examples
+#Units shipped needs to be pulled from NPTSLH file
 
 packages <- c('useful', 'coefplot', 'xgboost', 'here', 'magrittr', 'dygraphs', 'dplyr', 'RMySQL', 'caret', 'tinytex')
 purrr::walk(packages, library, character.only = TRUE)
@@ -13,6 +13,14 @@ query <- function(...) dbGetQuery(mychannel, ...)
 source('RMySQL_Update.R')
 
 sqlquery <- paste("SELECT 
+                  WAREHOUSE,
+                  PO_NUMBER,
+                  ITEM_NUMBER,
+                  RECEIPT_DATE,
+                  ETRN_NUMBER,
+                  DCI_MVTICK,
+                  PACK_TYPE,
+                  TOT_ALCLOC,
                   TOTAVL_QTY,
                   DAYS_FRM_SLE,
                   AVGD_BTW_SLE,
@@ -20,10 +28,12 @@ sqlquery <- paste("SELECT
                   SHIP_QTY_MN,
                   SHIP_QTY_SM,
                   SHIP_QTY_SD,
+                  UNITS_SHIPPED,
+                  AFT_REC_ALLOC,
                   PRIORITY
                   FROM
                   sandbox.pri_receipt
-                  WHERE RECEIPT_DATE <> '2019-01-14';", sep = "")
+                  WHERE RECEIPT_DATE <> '2019-01-15';", sep = "")
 data_pri <- query(sqlquery)
 
 set.seed(222)
@@ -43,7 +53,6 @@ priX_train <- build.x(data_formula_pri, data=dataTrain,
 priY_train <- build.y(data_formula_pri, data=dataTrain) %>% 
   as.integer()
 
-head(priY_train, n=100)
 
 
 priX_val <- build.x(data_formula_pri, data=dataTest,
@@ -61,14 +70,25 @@ xg9 <- xgb.train(
   nrounds=1000,
   eval_metric='logloss',
   watchlist=list(train=xgTrain, validate=xgVal),
-  print_every_n = 20, nthread=4,
+  print_every_n = 20, 
+  nthread=4,
+  eta=.1,
   early_stopping_rounds=70,
-  max_depth=3,
-  subsample=0.5, colsample_bytree=0.5,
+  max_depth=5,
+  subsample=0.5, 
+  colsample_bytree=0.5,
   num_parallel_tree=20
 )
 
 sqlquery <- paste("SELECT 
+                  WAREHOUSE,
+                  PO_NUMBER,
+                  ITEM_NUMBER,
+                  RECEIPT_DATE,
+                  ETRN_NUMBER,
+                  DCI_MVTICK,
+                  PACK_TYPE,
+                  TOT_ALCLOC,
                   TOTAVL_QTY,
                   DAYS_FRM_SLE,
                   AVGD_BTW_SLE,
@@ -76,15 +96,20 @@ sqlquery <- paste("SELECT
                   SHIP_QTY_MN,
                   SHIP_QTY_SM,
                   SHIP_QTY_SD,
+                  UNITS_SHIPPED,
+                  AFT_REC_ALLOC,
                   0 as PRIORITY
                   FROM
                   sandbox.pri_receipt
-                  WHERE RECEIPT_DATE = '2019-01-14';", sep = "")
+                  WHERE RECEIPT_DATE = '2019-01-15';", sep = "")
 preddata <- query(sqlquery)
 #need to build preddata as build.x
 data_new <- build.x(data_formula_pri, data=preddata, contrasts = FALSE, sparse = TRUE)
 preddata$PRIORITY <- predict(xg9,newdata = data_new)
-pred_values
+preddata
 
-
+data_new_test <- build.x(data_formula_pri, data=dataTest, contrasts = FALSE, sparse = TRUE)
+dataTest$PRIORITY_VAL <- predict(xg9,newdata = data_new_test)
+dataTest$PRED_DIF <- abs(dataTest$PRIORITY_VAL - dataTest$PRIORITY)
+dataTest
 
